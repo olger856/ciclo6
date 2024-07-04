3cead8a6a3046d6d320659c22e41d9d29f01987a
katie493 <katherine.aguirre@upeu.edu.pe>
2024-07-04 14:32:03 -0500

    proyect

--- evento_c6_app/lib/src/config/ConfigApi.dart
+++ evento_c6_app/lib/src/config/ConfigApi.dart
@@ -1,7 +1,11 @@
 class ConfigApi {
   static const String appName = "Colegio";
   // static const String apiURL = "172.22.4.12:9090";
+<<<<<<< HEAD
+  static const String apiURL = "192.168.56.1:9090";
+=======
   static const String apiURL = "172.17.96.1:9090";
+>>>>>>> f5feb8454d7ea105f349231508f3d4dfad2996d8
   static const String loginAPI = "/auth/login";
   static const String registerAPI = "/auth/create";
   static const String listUserAPI = "/auth/list";
